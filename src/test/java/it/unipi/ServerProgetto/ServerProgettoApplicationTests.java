04275b21cfa8f1b8a85b2c5fc18d566aaaad48b5
edoar <edoar@192.168.1.3>
2023-12-27 16:43:08 +0100

    Primo Scheletro

---
+++ src/test/java/it/unipi/ServerProgetto/ServerProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.ServerProgetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
